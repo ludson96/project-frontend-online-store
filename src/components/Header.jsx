27ff7f63c25e655afcb61d4cce10968193a8bc97
Vinicius Moraes <viniciusmasouza@hotmail.com>
2022-08-05 15:43:58 -0400

    Completa req 3

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Frontend Online Store</h1>
+        <nav className="header-nav">
+          <Link to="/">Home</Link>
+          <Link to="/shopping-cart">Carrinho de compras</Link>
+        </nav>
+      </div>
+    );
+  }
+}
