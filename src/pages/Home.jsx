7bad5285e2953751544ced15631ed3594645ed56
Vinicius Moraes <viniciusmasouza@hotmail.com>
2022-08-05 18:30:25 -0400

    Completa req 5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,68 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchInputValue: '',
+      itens: [],
+      resultsFound: true,
+    };
+  }
+
+  searchInputOnChange = ({ target }) => {
+    this.setState({ searchInputValue: target.value });
+  }
+
+  searchButtonOnClick = async () => {
+    const { searchInputValue } = this.state;
+    const itens = await getProductsFromCategoryAndQuery('', searchInputValue);
+    if (itens.length === 0) this.setState({ resultsFound: false });
+    if (itens.length !== 0) this.setState({ resultsFound: true });
+    this.setState({ itens });
+  }
+
   render() {
+    const { searchInputValue, itens, resultsFound } = this.state;
     return (
       <div className="home">
         <div className="search-field">
-          <input type="text" className="search-input" />
-          <button type="submit" className="search-btn">Pesquisar</button>
+          <input
+            data-testid="query-input"
+            type="text"
+            className="search-input"
+            value={ searchInputValue }
+            onChange={ this.searchInputOnChange }
+          />
+          <button
+            data-testid="query-button"
+            type="button"
+            className="search-btn"
+            onClick={ this.searchButtonOnClick }
+          >
+            Pesquisar
+          </button>
         </div>
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
+        {resultsFound ? (
+          <div className="itens-container">
+            {itens.map((iten) => (
+              <div data-testid="product" key={ iten.id } className="iten">
+                <h3>{iten.title}</h3>
+                <img src={ iten.thumbnail } alt={ iten.title } />
+                <p>
+                  RS
+                  {iten.price}
+                </p>
+              </div>
+            ))}
+          </div>
+        ) : <p>Nenhum produto foi encontrado</p>}
+
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -7,5 +7,5 @@ export async function getCategories() {
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
   const response = await data.json();
-  return response;
+  return response.results;
 }
