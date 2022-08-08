b5fa60ec98491408d4376f1f45520041ceb8852c
Vinicius Moraes <viniciusmasouza@hotmail.com>
2022-08-08 19:31:17 -0400

    Completa req 9

--- src/pages/ShopingCart.jsx
+++ src/pages/ShopingCart.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class ShoppingCart extends Component {
   state = {
@@ -16,20 +17,23 @@ export default class ShoppingCart extends Component {
     const { cartList } = this.state;
     return (
       <div>
-        { cartList.length === 0
-          ? (
-            <p data-testid="shopping-cart-empty-message">
-              Seu carrinho está vazio
-            </p>
-          )
-          : cartList.map((item) => (
-            <li key={ item.id }>
-              <h3 data-testid="shopping-cart-product-name">{ item.title }</h3>
-              <img src={ item.thumbnail } alt={ item.title } />
-              <p>{ item.price }</p>
-              <p data-testid="shopping-cart-product-quantity">{ item.quant }</p>
-            </li>
-          ))}
+        <Header />
+        <div>
+          { cartList.length === 0
+            ? (
+              <p data-testid="shopping-cart-empty-message">
+                Seu carrinho está vazio
+              </p>
+            )
+            : cartList.map((item) => (
+              <li key={ item.id }>
+                <h3 data-testid="shopping-cart-product-name">{ item.title }</h3>
+                <img src={ item.thumbnail } alt={ item.title } />
+                <p>{ item.price }</p>
+                <p data-testid="shopping-cart-product-quantity">{ item.quant }</p>
+              </li>
+            ))}
+        </div>
       </div>
     );
   }
